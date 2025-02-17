83c28c98e1d1fe8242dd90625690763ca508271b
Phiefan <miatho13@outlook.com>
2025-02-17 14:39:23 +0100

    Added a parameterized test for translation of a letter to morse code and needed implementation, such as, data for MethodSource, constructor and the method translateToMorseCode

--- src/submission/MorseCodeTranslator.java
+++ src/submission/MorseCodeTranslator.java
@@ -1,4 +1,23 @@
 package submission;
 
+import java.util.HashMap;
+
 public class MorseCodeTranslator {
+
+    private final HashMap<String, String> alphabet;
+
+    public MorseCodeTranslator(){
+        String[] english = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+        String[] morseCode = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+
+        this.alphabet = new HashMap<>();
+
+        for (int i = 0; i < 26; i++) {
+            this.alphabet.put(english[i], morseCode[i]);
+        }
+    }
+    
+    public String translateToMorseCode(String str) {
+        return this.alphabet.get(str);
+    }
 }
--- src/submission/MorseCodeTranslatorTests.java
+++ src/submission/MorseCodeTranslatorTests.java
@@ -1,15 +1,61 @@
 package submission;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class MorseCodeTranslatorTests {
 
     private MorseCodeTranslator translator;
+
+    public static Stream<Arguments> testData() {
+        return Stream.of(
+                arguments("A", ".-"),
+                arguments("B", "-..."),
+                arguments("C", "-.-."),
+                arguments("D", "-.."),
+                arguments("E", "."),
+                arguments("F", "..-."),
+                arguments("G", "--."),
+                arguments("H", "...."),
+                arguments("I", ".."),
+                arguments("J", ".---"),
+                arguments("K", "-.-"),
+                arguments("L", ".-.."),
+                arguments("M", "--"),
+                arguments("N", "-."),
+                arguments("O", "---"),
+                arguments("P", ".--."),
+                arguments("Q", "--.-"),
+                arguments("R", ".-."),
+                arguments("S", "..."),
+                arguments("T", "-"),
+                arguments("U", "..-"),
+                arguments("V", "...-"),
+                arguments("W", ".--"),
+                arguments("X", "-..-"),
+                arguments("Y", "-.--"),
+                arguments("Z", "--..")
+        );
+    }
 
     @BeforeEach
     public void init(){
         translator = new MorseCodeTranslator();
     }
 
+    @ParameterizedTest
+    @MethodSource("testData")
+    public void testLetterToMorseCode(String input, String expected){
+        String actual = translator.translateToMorseCode(input);
+
+        assertEquals(expected, actual);
+    }
 
 }
